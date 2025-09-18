34b7aa8b9d84695e6154e217dcddd6805eea0cda
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-09-18 20:16:29 +0300

    Projection

--- DemoLINQ/Program.cs
+++ DemoLINQ/Program.cs
@@ -26,6 +26,49 @@ namespace DemoLINQ
             // Search in the first 10 products only
             // Indexed Where Valid only with Fluent Syntax , Cannot be used with Query Syntax
             #endregion
+            #region Transformation [Projection] Operators [Select ,SelectMany]
+            // First overload of Select method 
+            //var productsNames = ProductsList.Select(p=>p.ProductName);
+            //foreach (var item in productsNames)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            // Query Syntax 
+            //var productsNames = from p in ProductsList
+            //                    select p.ProductName;
+            ///*******************/
+            // Select Product Id and Product Name 
+            //var result = ProductsList.Select(p => new { p.ProductID, p.ProductName });
+            //result = from p in ProductsList
+            //              select new { p.ProductID, p.ProductName };
+            ///*******************/
+            // Select Product In Stock and Apply Discount 10% on its Price 
+            //var result = ProductsList.Where(p=>p.UnitsInStock>0)
+            //                         .Select(p => new
+            //                         {
+            //                             p.ProductID,
+            //                             p.ProductName,
+            //                             p.UnitPrice,
+            //                             PriceAfterDiscount = p.UnitPrice * 0.1m
+            //                         });
+
+            /// ///*******************/
+            // Second overload of Select method => Indexed Select  valid only with Fluent Syntax , Cannot be used with Query Syntax
+            //var productsNames = ProductsList.Where(p => p.UnitsInStock > 0).Select((p, i) => new
+            //{
+            //    Index = i ,
+            //    Name = p.ProductName,
+            //});
+
+            ///*******************/
+            // SelectCustomer Orders 
+            // using SelectMany method Select Customer of arr [orders]
+            //var result = CustomersList.SelectMany(c => c.Orders);
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
             #endregion
         }
     }
