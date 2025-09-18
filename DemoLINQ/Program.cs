cec853eb593d07cabd298a21da019bd217bd0897
Mohamed Mahmoud Abdel Aleem <mohamedmahamoudali15@gmail.com>
2025-09-18 19:08:13 +0300

    Add project files.

---
+++ DemoLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using static DemoLINQ.ListGenerator;
+namespace DemoLINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Demo Session 01 LINQ 
+            #endregion
+        }
+    }
+}
